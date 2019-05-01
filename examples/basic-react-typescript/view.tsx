485abc5cafd3f997f927e485449381c422c127ee
Johan Brook <johan@lookback.io>
2019-05-01 23:42:16 +0200

    Remove use of React Context from example

--- examples/basic-react-typescript/view.tsx
+++ examples/basic-react-typescript/view.tsx
@@ -16,34 +16,17 @@ export interface FetchUserAgent {
 /** Types of messages the view can send. */
 export type ViewIn = DidClickButton | FetchUserAgent;
 
-export type Send = (event: ViewIn) => void;
-
-/**
- * Context exposed to all components via <StateContext.Consumer>
- */
-export interface Context {
-    /** Our app state */
-    state: MyState;
-    /** Function to send messages with */
-    send: Send;
-}
-
-/** Helper to create a new Context for the <StateContext.Provider> */
-export const makeContext = (state: MyState, send?: Send | null): Context => ({
-    state,
-    send: send || (() => {}), // Pass noop as default Send function
-});
-
-export const StateContext = React.createContext<Context>(
-    makeContext(startState)
-);
+/** Our 'send' function signature. */
+type Send = (event: ViewIn) => void;
 
 const App = () => {
     const [state$, setState] = useState<Stream<MyState>>(xs.empty());
 
     const viewIn$ = xs.create<ViewIn>();
 
-    const send = useCallback((v: ViewIn) => {
+    // Create our actual 'send' function which drives the viewIn$
+    // stream, which goes into `main`.
+    const send: Send = useCallback((v: ViewIn) => {
         viewIn$.shamefullySendNext(v);
     }, []);
 
@@ -54,26 +37,24 @@ const App = () => {
     }, []);
     
     // 'ourState' now includes our whole app state as a plain object
-    const ourState = useStream(state$, startState);
-    // …use that to create our React context
-    const context = makeContext(ourState, send);
+    const state = useStream(state$, startState);
 
     return (
-        <StateContext.Provider value={context}>
+        <div>
             <p>
                 <button
-                    onClick={() => context.send({
+                    onClick={() => send({
                         kind: 'did_click_button',
                         didClick: true,
                     } as DidClickButton)}>
-                        'Click this button'
+                        Click this button
                     </button>
             </p>
 
             <p>
                 <button 
-                    disabled={!!context.state.userAgent}
-                    onClick={() => context.send({
+                    disabled={!!state.userAgent}
+                    onClick={() => send({
                         kind: 'fetch_user_agent',
                     } as FetchUserAgent)}>
                     Fetch browser user agent
@@ -81,8 +62,8 @@ const App = () => {
             </p>
 
             <h4>Our current state:</h4>
-            <pre>{JSON.stringify(context.state, null, 4)}</pre>
-        </StateContext.Provider>
+            <pre>{JSON.stringify(state, null, 4)}</pre>
+        </div>
     );
 };
 
